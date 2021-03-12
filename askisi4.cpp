105f4ef39a9802c8dd5982c39ee96334004a8f9f
DAWGPerkyS <jimmy.makro24@gmail.com>
2021-03-12 12:57:30 +0200

    askisi4,5 with functions

--- askisi4.cpp
+++ askisi4.cpp
@@ -1,18 +1,37 @@
 #include "std_lib_facilities.h"
 
+string getinput();
+void string_check(vector<char> a, vector<int> b, string c);
+
 int main() {
+    
+    string str = getinput();
+    cout << endl;
+
+    //Making a vector for checking perantheses
+    vector<char> parenth;
+    //Making vector for getting positions
+    vector<int> pos;
+    
+
+    string_check(parenth, pos, str);
+
+
+    keep_window_open();
+    return 0;
+}
+
+string getinput() {
     cout << "Type a string!" << endl;
     cout << "=>" ;
     string str;
     //Input string
     getline(cin, str);
-    cout << endl;
-    //Making a vector for checking perantheses
-    vector<char> parenth;
-    //Making vector for getting positions
-    vector<int> pos;
-    int i{0}; 
+    return str;
+}
 
+void string_check(vector<char> parenth, vector<int> pos, string str) {
+    int i{0}; 
     while (i < str.size()) {
         //Pushing back left parenthesis in the vector
         if (str.at(i) == '(') {
@@ -27,12 +46,12 @@ int main() {
             //If there isnt a left parenthesis theres output for wrong parenthesis 
             } else {
                 cout << "Wrong at position " << i << '.' << endl;
-                // keep_window_open();
-                return 0;
+                break;
             }
         }
         i++;    
     }
+
     //Checking vector so there isnt any parentheses left inside
     //That means the string is correct
     if (0 == parenth.size()) {
@@ -42,9 +61,4 @@ int main() {
     } else {
         cout << "Wrong at position " << pos.back() << '.' << endl;
     }
-
-    // keep_window_open();
-    return 0;
 }
-
-
--- askisi5.cpp
+++ askisi5.cpp
@@ -1,69 +1,40 @@
 #include "std_lib_facilities.h"
 
+int getcoins();
+void pushbacknoms(int a, int b, int c, vector<int> d);
+double getcost();
+int getbill();
+int remaining(int e);
+void calculate_change(vector<int> d, int e);
 
 int main() {
   // Get available coins
   cout << "Please insert the available currency coins: ";
-  int nom1, nom2, nom3;
+  int nom1 = getcoins(), nom2 = getcoins(), nom3 = getcoins();
+  
   vector<int> nomism; 
-  cin >> nom1 >> nom2 >> nom3;
-  if (!cin) {
-    throw runtime_error("Wrong input!");
-  }
+  pushbacknoms(nom1, nom2,nom3, nomism);
 
-  // Insert coins in a vector and sort it
-  nomism.push_back(nom1);
-  nomism.push_back(nom2);
-  nomism.push_back(nom3);
-  sort(nomism.begin(), nomism.end());
+ 
 
+  
   // Begin loop
   while (true) {
     // Get product cost
-    cout << "Please insert the cost of the product (insert -1 to stop): ";
-    double cost;
-    cin >> cost;
-    if (!cin) {
-      throw runtime_error("Wrong input!");
-      /* cout << "Wrong input!";
-      keep_window_open();
-      return -1; */
-    }
+    double cost = getcost();
 
     // If cost == -1 stop the loop
     if (cost == -1) {
       break;
     }
 
-    // Calculate remaining ammount as cents (* 100)
-    int cost_a = cost * 100;
+    int cost_a = remaining(cost); 
 
-    // While there is a remaining ammount
-    while (cost_a > 0) {
-      int bill;
-      cout << "Please pay using a 200, 500 or 1000 bill: ";
-      cin >> bill;
-      if (!cin) {
-        throw runtime_error("Wrong input!");
-      }
-      // Accept only specific bills
-      if (bill == 500 || bill == 200 || bill == 1000) {
-        cost_a -= bill;
-      }
-    }
-
-    // Calculate change
-    cost_a *= -1;
     
     // Calculate change from each denomination
     cout << "Your change is ";
-    for (int i = nomism.size() - 1; i >= 0; i--) {
-      // Integet division => number of coins
-      cout << nomism[i] << "x" << cost_a / nomism[i] << " ";
-      // Modulo => remaining cost
-      cost_a %= nomism[i];
-    }
-    cout << endl;
+    calculate_change(nomism, cost_a);
+    
 
     // Chnage that we kept
     if (cost_a > 0) {
@@ -75,3 +46,73 @@ int main() {
   keep_window_open();
   return 0;
 }
+
+int getcoins() {
+  int nom;
+  cin >> nom;
+  if (!cin) {
+    throw runtime_error("Wrong input!");
+  }
+  return nom;
+}
+
+void pushbacknoms(int nom1, int nom2, int nom3, vector<int> nomism) {
+  // Insert coins in a vector and sort it
+  nomism.push_back(nom1);
+  nomism.push_back(nom2);
+  nomism.push_back(nom3);
+  sort(nomism.begin(), nomism.end());
+ } 
+
+double getcost() {
+  cout << "Please insert the cost of the product (insert -1 to stop): ";
+    double cost;
+    cin >> cost;
+    if (!cin) {
+      throw runtime_error("Wrong input!");
+      
+      /* cout << "Wrong input!";
+      keep_window_open();
+      return -1; */
+    }
+
+  return cost;
+}
+
+int getbill() {
+   int bill;
+    cout << "Please pay using a 200, 500 or 1000 bill: ";
+    cin >> bill;
+    if (!cin) {
+      throw runtime_error("Wrong input!");
+    }
+    return bill;
+ }
+
+ int remaining(int cost) {
+
+   int cost_a = cost * 100;
+
+    // While there is a remaining ammount
+    while (cost_a > 0) {
+      int bill = getbill();
+     
+      // Accept only specific bills
+      if (bill == 500 || bill == 200 || bill == 1000) {
+        cost_a -= bill;
+      }
+    }
+
+    // Calculate change
+    return cost *= -1;
+ }
+
+void calculate_change(vector<int> nomism, int cost_a) {
+  for (int i = nomism.size() - 1; i >= 0; i--) {
+      // Integet division => number of coins
+      cout << nomism[i] << "x" << cost_a / nomism[i] << " ";
+      // Modulo => remaining cost
+      cost_a %= nomism[i];
+  }
+    cout << endl;
+ }
